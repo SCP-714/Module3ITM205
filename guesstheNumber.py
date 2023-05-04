f9584730de6ba63de45974bf957395a7c0aa8100
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-05-04 16:07:24 -0500

    refactored guessing game

--- guesstheNumber.py
+++ guesstheNumber.py
@@ -3,7 +3,11 @@ guessCount = 0
 guessLimit = 3;
 
 while guessCount < guessLimit:
-    guess = int(input("Guess a number between 1 and 10: "));
+    guess = int(input("Guess a number between 1 and 10 you have 3 chances please begin: "));
     guessCount += 1;
     if guess == secretNumber:
         print('You got it! You are awesome!')
+        break
+    else:
+        print('I am sorry you did not get it please try again!');
+
